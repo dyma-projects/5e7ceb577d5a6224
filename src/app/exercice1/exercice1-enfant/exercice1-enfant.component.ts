a0a9e1176d4eb0661b9037297e1740665764be46
Fabrice Quinio <fquinio@oxymelfr.com>
2020-04-03 11:07:14 +0200

    Projets 3 (chapitre 4)

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
   templateUrl: './exercice1-enfant.component.html',
   styleUrls: ['./exercice1-enfant.component.css']
 })
-export class Exercice1EnfantComponent implements OnInit {
+export class Exercice1EnfantComponent implements OnInit {  
+  
+  @Input() compteur;
+  @Output("compteurChangementEnfant") changementCompteur = new EventEmitter();
 
   constructor() { }
+
+  public incrementerCompteur() : void {
+    this.compteur ++;    
+    this.changementCompteur.emit({value : this.compteur});
+  }
+
+  public decrementerCompteur() : void {
+    this.compteur --;    
+    this.changementCompteur.emit({value : this.compteur});
+  }
 
   ngOnInit() {
   }
--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class Exercice1Component implements OnInit {
   public compteur: number = 0; 
 
+  public miseAJourCompteur(event) : void {
+    this.compteur = event.value;
+  }
   constructor() { }
 
   ngOnInit() {
